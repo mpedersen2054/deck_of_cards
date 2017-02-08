4663bfa61b4e1769ccaeb6dc5e24483e3989f9c9
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-02-07 20:11:37 -0800

    finish Card class

--- Card.cs
+++ Card.cs
@@ -5,9 +5,14 @@ namespace ConsoleApplication
 {
     public class Card
     {
-        public Card()
+        public string stringVal;
+        public string suit;
+        public string val;
+        public Card(string stringVal, string suit, int val)
         {
-            
+            this.stringVal = stringVal;
+            this.suit = suit;
+            this.val = val;
         }
     }
 }
